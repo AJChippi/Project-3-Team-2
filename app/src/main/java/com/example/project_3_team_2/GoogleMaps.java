1fd91bc290a42e21f727fb57f988d9fa9553da4d
Nathan <nlvansne@svsu.edu>
2022-12-11 14:40:24 -0500

    Final push!!!!
    Signed-off-by: Nathan <nlvansne@svsu.edu>

--- app/src/main/java/com/example/project_3_team_2/GoogleMaps.java
+++ app/src/main/java/com/example/project_3_team_2/GoogleMaps.java
@@ -1,31 +1,108 @@
 package com.example.project_3_team_2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import com.google.android.gms.maps.GoogleMap;
 
-public class GoogleMaps extends FragmentActivity implements GoogleMap.OnMapLoadedCallback {
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    private GoogleMap mMap;
+import java.util.Comparator;
+
+public class GoogleMaps extends AppCompatActivity implements OnMapReadyCallback {
+
+    GoogleMap map;
+    RequestQueue queue;
+    final String TAG = "GOOGLEMAPS";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_maps);
+        queue = Volley.newRequestQueue(this);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
     }
 
     @Override
-    public void onMapLoaded() {
+    public void onMapReady(@NonNull GoogleMap googleMap) {
+        map = googleMap;
+        //Set the camera to svsu
+        LatLng svsu = new LatLng(42.7325, -84.5555);
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(svsu,7));
+        SharedPreferences sharedPreferences = getSharedPreferences("my_prefs", MODE_PRIVATE);
+        Log.d("shared", sharedPreferences.getString("userID", ""));
+        String url = "https://findtutors.onrender.com/tutorList?userID="+sharedPreferences.getString("userID", "");
 
+        map.setOnInfoWindowClickListener(marker -> {
+            Intent intent = new Intent(this, TutorInformation.class);
+            intent.putExtra("userID", marker.getTag().toString());
+            Log.d(TAG, "marker: "+marker.getTag().toString());
+            startActivity(intent);
+        });
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET,url,null,
+                response->{
+                    try {
+
+                        JSONArray jsonArray = response.getJSONArray("results");
+                        for(int i = 0; i < jsonArray.length()-1; i++){
+                            JSONObject jsonObject = jsonArray.getJSONObject(i);
+                            Log.d(TAG, "onMapReady: "+jsonObject);
+                            String longitude ="";
+                            String latitude = "";
+                            try {
+                                longitude = jsonObject.getString("longitude");
+                                latitude = jsonObject.getString("latitude");
+                            }catch (Exception e){
+                                Log.d(TAG, "eororor: "+e);
+                            }
+                            Log.d(TAG,"LONGITUDE" +longitude);
+                            String name = jsonObject.getString("name");
+                            String subject = jsonObject.getString("subject");
+                            String description = jsonObject.getString("bio");
+                            String userID = jsonObject.getString("userID");
+                            try {
+                                LatLng tutorLocation = new LatLng(Double.parseDouble(latitude), Double.parseDouble(longitude));
+                                Log.d(TAG, "i wanna die" + tutorLocation.toString());
+                                Marker m = map.addMarker(new MarkerOptions().position(tutorLocation).title(name).snippet("Subject: " + subject + " : Description: " + description));
+                                m.setTag(userID);
+                            }catch (Exception ex){
+                                ex.printStackTrace();
+                            }
+                        }
+                    } catch (Exception e) {
+                        Log.d(TAG, e.toString());
+                        e.printStackTrace();
+                    }
+                },error->{
+            Log.d(TAG,"error: " + error.getMessage());
+        });
+
+        queue.add(request);
     }
 }
--- app/src/main/java/com/example/project_3_team_2/ListViewActivity.java
+++ app/src/main/java/com/example/project_3_team_2/ListViewActivity.java
@@ -17,6 +17,7 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.tasks.OnSuccessListener;
 
 import android.util.Log;
@@ -25,6 +26,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -34,6 +36,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import com.google.maps.android.SphericalUtil;
 
 public class ListViewActivity extends AppCompatActivity {
     private static final int REQUEST_LOCATION_PERMISSION = 1;
@@ -41,6 +44,7 @@ public class ListViewActivity extends AppCompatActivity {
     Spinner spnListFilter;
     ArrayList<Tutor> tutors;
     TutorAdapter adapter;
+    TextView txtDistance;
     RequestQueue queue;
     SharedPreferences preferences;
     String curUserID;
@@ -56,6 +60,7 @@ public class ListViewActivity extends AppCompatActivity {
         queue = Volley.newRequestQueue(this);
 
         lstTutors = findViewById(R.id.lstTutors);
+        txtDistance = findViewById(R.id.txtDistance);
         spnListFilter = findViewById(R.id.spnListFilter);
         tutors = new ArrayList<>();
         adapter = new TutorAdapter(tutors,this);
@@ -77,8 +82,8 @@ public class ListViewActivity extends AppCompatActivity {
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET,url,null,
                 response->{
                     try{
-                        Log.d(TAG,response.getJSONArray("results").getJSONArray(0)+"");
-                        JSONArray jsonTutors = response.getJSONArray("results").getJSONArray(0);
+                        Log.d(TAG,response.getJSONArray("results").toString());
+                        JSONArray jsonTutors = response.getJSONArray("results");
                         for (int i = 0; i < jsonTutors.length()-1; i++){
                             JSONObject t = jsonTutors.getJSONObject(i);
                             String userID = t.getString("userID");
@@ -93,12 +98,14 @@ public class ListViewActivity extends AppCompatActivity {
                                 longitude =0;
                             }
 
-                            // Get distance to tutor
-                            float[] result = new float[1];
-                            Location.distanceBetween(deviceLat,deviceLng,latitude,longitude,result);
-                            float distance = result[0];
+                            LatLng tutorLoc = new LatLng(latitude,longitude);
+                            LatLng deviceLoc = new LatLng(deviceLat,deviceLng);
 
-                            Tutor nextTutor = new Tutor(userID,name,subject,latitude,longitude,distance);
+                            double distance = SphericalUtil.computeDistanceBetween(tutorLoc, deviceLoc);
+                            double total = distance / 1609.344;
+                            //double distance = distance(latitude,longitude,deviceLat,deviceLng);
+
+                            Tutor nextTutor = new Tutor(userID,name,subject,latitude,longitude, (float) total);
                             tutors.add(nextTutor);
                         }
                         tutors.sort(Comparator.reverseOrder());
@@ -112,10 +119,10 @@ public class ListViewActivity extends AppCompatActivity {
 
         queue.add(request);
 
-        tutors.add(new Tutor("1","Example Name1","Math",1,1,10));
+       /* tutors.add(new Tutor("1","Example Name1","Math",1,1,10));
         tutors.add(new Tutor("2","Example Name2","Computer Science",1,1,20));
         tutors.add(new Tutor("3","Example Name3","English",1,1,40));
-        tutors.add(new Tutor("3","Example Name4","Math",1,1,30));
+        tutors.add(new Tutor("3","Example Name4","Math",1,1,30));*/
 
         // Sort the list
 //        Collections.sort(tutors);
